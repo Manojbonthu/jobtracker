feac1e46f63997d7e6744686fb83f164a9201dfa
Bonthu Manoj <116989628+Manojbonthu@users.noreply.github.com>
2026-03-20 15:48:07 +0530

    Update models.py

--- models.py
+++ models.py
@@ -2,14 +2,15 @@ from sqlalchemy import Column, Integer, String, Text, TIMESTAMP
 from sqlalchemy.sql import func
 from database import Base
 
-
 class JobPost(Base):
     __tablename__ = "job_posts"
-
-    id = Column(Integer, primary_key=True, index=True)
-    job_role = Column(String(255), nullable=True)
-    job_location = Column(String(255), nullable=True)
+    id            = Column(Integer, primary_key=True, index=True)
+    job_role      = Column(String(255), nullable=True)
+    company_name  = Column(String(255), nullable=True)
+    job_location  = Column(String(255), nullable=True)
     contact_email = Column(String(255), nullable=True)
+    apply_link    = Column(Text, nullable=True)
+    salary        = Column(String(255), nullable=True)
     job_description = Column(Text, nullable=True)
-    raw_post = Column(Text, nullable=True)
-    date_added = Column(TIMESTAMP, server_default=func.now())
+    raw_post      = Column(Text, nullable=True)
+    date_added    = Column(TIMESTAMP, server_default=func.now())
